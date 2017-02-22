707e373bc804042f8049a690932919bc311c1190
James Fisher-Martins <james@claro.pt>
2017-02-22 12:47:25 +0000

    Updated models and fixed schemas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :gigs, :foreign_key => :host_id
+  has_many :bookings
+
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable,
   :omniauthable, omniauth_providers: [:facebook]
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222112748) do
+ActiveRecord::Schema.define(version: 20170222114838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "gig_id"
+    t.integer  "quantity"
+    t.string   "rating_description"
+    t.integer  "rating"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["gig_id"], name: "index_bookings_on_gig_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
 
   create_table "gigs", force: :cascade do |t|
     t.string   "name"
@@ -52,4 +64,6 @@ ActiveRecord::Schema.define(version: 20170222112748) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "bookings", "gigs"
+  add_foreign_key "bookings", "users"
 end
